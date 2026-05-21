cada7fd944eca86f1509c1d4d4c0920dbca12dd2
EmilyMacedo278 <emily27289@Gmail.com>
2026-05-21 13:59:56 -0300

    Tentando reajustar o Swagger

--- onepiece-api/src/main/resources/data.sql
+++ onepiece-api/src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO crews (name, ship_name, origin_sea) VALUES ('Chapéus de Palha', 'Thousand Sunny', 'East Blue');
-INSERT INTO crews (name, ship_name, origin_sea) VALUES ('Piratas do Ruivo', 'Red Force', 'West Blue');
-INSERT INTO crews (name, ship_name, origin_sea) VALUES ('Piratas Heart', 'Polar Tang', 'North Blue');
+INSERT INTO crew (name, ship_name, origin_sea) VALUES ('Chapeus de Palha', 'Thousand Sunny', 'East Blue');
+INSERT INTO crew (name, ship_name, origin_sea) VALUES ('Piratas do Ruivo', 'Red Force', 'West Blue');
+INSERT INTO crew (name, ship_name, origin_sea) VALUES ('Piratas Heart', 'Polar Tang', 'North Blue');
 
-INSERT INTO devil_fruits (name, type, power_description) VALUES ('Gomu Gomu no Mi', 'Paramecia', 'Permite ao usuário ter um corpo de borracha.');
-INSERT INTO devil_fruits (name, type, power_description) VALUES ('Ope Ope no Mi', 'Paramecia', 'Permite criar uma área de controle cirúrgico.');
-INSERT INTO devil_fruits (name, type, power_description) VALUES ('Mera Mera no Mi', 'Logia', 'Permite criar, controlar e se transformar em fogo.');
+INSERT INTO devil_fruit (name, type, power_description) VALUES ('Gomu Gomu no Mi', 'Paramecia', 'Corpo de borracha.');
+INSERT INTO devil_fruit (name, type, power_description) VALUES ('Ope Ope no Mi', 'Paramecia', 'Cria uma area de controle cirurgico.');
+INSERT INTO devil_fruit (name, type, power_description) VALUES ('Mera Mera no Mi', 'Logia', 'Controla e se transforma em fogo.');
 
-INSERT INTO characters (name, role, bounty, status, crew_id, devil_fruit_id) VALUES ('Monkey D. Luffy', 'Capitão', '3.000.000.000', 'Ativo', 1, 1);
-INSERT INTO characters (name, role, bounty, status, crew_id, devil_fruit_id) VALUES ('Trafalgar Law', 'Capitão/Médico', '3.000.000.000', 'Ativo', 3, 2);
+INSERT INTO characters (name, role, bounty, status, crew_id, devil_fruit_id) VALUES ('Monkey D. Luffy', 'Capitao', '3.000.000.000', 'Ativo', 1, 1);
+INSERT INTO characters (name, role, bounty, status, crew_id, devil_fruit_id) VALUES ('Trafalgar Law', 'Capitao/Medico', '3.000.000.000', 'Ativo', 3, 2);
 INSERT INTO characters (name, role, bounty, status, crew_id, devil_fruit_id) VALUES ('Portgas D. Ace', 'Comandante', '550.000.000', 'Falecido', 1, 3);
